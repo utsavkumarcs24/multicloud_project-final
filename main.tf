754881670a95b190c0dbc0045add265700b072f5
UTSAV KUMAR <265995441+utsavkumarcs24@users.noreply.github.com>
2026-03-14 00:18:33 +0530

    Add S3 bucket resource configuration

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-first-utsav24"  # Change to a globally unique name
+
+  tags = {
+    Name        = "MyTerraformBucket"
+    Environment = "Dev"
+  }
+}
