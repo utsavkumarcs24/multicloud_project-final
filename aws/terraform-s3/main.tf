e527e7a819dbea1526d41b0912ce4d4b1deda374
UTSAV KUMAR <265995441+utsavkumarcs24@users.noreply.github.com>
2026-03-14 00:14:49 +0530

    Rename S3 bucket to a globally unique name

--- aws/terraform-s3/main.tf
+++ aws/terraform-s3/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-first-UTSAVKUMAR234j"  # Change to a globally unique name
+  bucket = "my-first-utsav24"  # Change to a globally unique name
 
   tags = {
     Name        = "MyTerraformBucket"
